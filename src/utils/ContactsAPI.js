87f5de139cdce33265887cef762a2c50959587ee
shalurai <shaluraics22@gmail.com>
2017-07-29 22:36:37 +0900

    API Address changed

--- src/utils/ContactsAPI.js
+++ src/utils/ContactsAPI.js
@@ -1,4 +1,4 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:5001'
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://139.59.78.205:5001'
 
 let token = localStorage.token
 
